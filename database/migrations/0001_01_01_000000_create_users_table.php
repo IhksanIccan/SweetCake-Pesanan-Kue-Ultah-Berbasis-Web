fde52ab80d74ef9015c5f963c79ff55b41e43175
Ihksan Iccan <ihksan2207@gmail.com>
2025-05-05 14:45:04 +0800

    Initial commit - add Laravel project files

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,9 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id(); //
+            $table->id(); 
             $table->string('name');
-            $table->string('email')->unique(); //
+            $table->string('email')->unique(); 
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password'); // Password terenkripsi
             $table->enum('role', ['admin', 'customer', 'staff']); // Role pengguna (admin, customer, staff)
--- database/migrations/2025_05_01_091332_create_pesanans_table.php
+++ database/migrations/2025_05_01_091332_create_pesanans_table.php
@@ -13,8 +13,9 @@ return new class extends Migration
     {
         Schema::create('pesanans', function (Blueprint $table) {
             $table->id();
-            
+
         });
+        
     }
 
     /**
